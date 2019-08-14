cf4f996b240dfef88ed838573345e315de0089c9
Dzf <490758749@qq.com>
2019-08-14 09:06:12 +0800

    基础框架搭建

---
+++ src/pages/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main">
+      <!--头部-->
+      <Header></Header>
+  </div>
+</template>
+
+<script>
+  import Header from './../components/Header';
+  export default {
+    name: 'Index',
+    components:{
+      Header
+    },
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "./../assets/styles/mixin";
+</style>
